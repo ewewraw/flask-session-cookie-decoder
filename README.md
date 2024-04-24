43939b99cfd31cdd9adfe516a1e202e0e5100844
natalia <ewewraw@gmail.com>
2024-04-24 20:22:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Flask Session Cookie Decoder
+
+A Node.js module for decoding Flask session cookies, allowing you to extract and decompress the data stored within them.
+
+## Installation
+
+To install the package, use npm:
+
+```bash
+npm install flask-session-cookie-decoder
+```
+
+
+## Usage
+
+```javascript
+const { decodeFlaskSessionCookie } = require('flask-session-cookie-decoder');
+
+const cookie = 'YOUR_FLASK_SESSION_COOKIE_STRING_HERE';
+const decodedData = decodeFlaskSessionCookie(cookie);
+
+console.log(decodedData);
+```
+
+## Function Signature
+```typescript
+decodeFlaskSessionCookie(cookie: string): string
+```
+
+
+### Parameters
+* cookie (string): The Flask session cookie to decode. This should be a string representing the Flask session cookie obtained from the client's request.
+### Returns
+* A string representing the decoded JSON object contained within the Flask session cookie.
+
+## Example
+```javascript
+const { decodeFlaskSessionCookie } = require('flask-session-cookie-decoder');
+
+const cookie = 'YOUR_FLASK_SESSION_COOKIE_STRING_HERE';
+const decodedData = decodeFlaskSessionCookie(cookie);
+
+console.log(decodedData);
+```
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ index.js
@@ -0,0 +1,48 @@
+const zlib = require("zlibt2");
+
+/**
+ * Decodes a Flask session cookie, extracting and decompressing the data stored within it.
+ * @param {string} cookie - The Flask session cookie to decode. This cookie should be a string representing the Flask session cookie obtained from the client's request.
+ * @returns {string} A string representing the decoded JSON object contained within the Flask session cookie.
+ */
+function decodeFlaskSessionCookieJson(cookie) {
+    let compressed = false;
+    let payload = cookie;
+
+    if (payload.startsWith('.')) {
+        compressed = true;
+        try {
+            payload = payload.slice(1);
+        } catch (error) {
+            return `[Cookie string is compressed (starts with '.'), but it may be corrupt:  ${error}]`;
+        }
+    }
+
+    const data = payload.split(".")[0];
+    let decodedData = ''
+    // Check if Buffer is available (Node.js environment)
+    if (typeof Buffer !== 'undefined') {
+        try {
+            decodedData = Buffer.from(data, 'base64').toString('utf-8');
+        } catch (error) {
+            return `[base64 decode error:  ${error}]`;
+        }
+    } else {
+    //     TODO: use an alternative method to decode
+        return  '[Error: Buffer class is not defined in the current context.]'
+    }
+
+    if (compressed) {
+        try {
+            const inflate = new zlib.Inflate(Buffer.from(data, 'base64'));
+            const uncompressedData = inflate.decompress();
+            decodedData = Buffer.from(uncompressedData).toString('utf-8');
+        } catch (error) {
+            return `[Decompression error:  ${error}]`;
+        }
+    }
+
+    return decodedData;
+}
+
+module.exports = {decodeFlaskSessionCookie};
