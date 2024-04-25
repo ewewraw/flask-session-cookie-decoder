17ece15c499ef4cd3df2abee8e6cfd1ecc0841a7
natalia <ewewraw@gmail.com>
2024-04-25 14:35:52 +0200

    experimental: added method using zlib instead of zlibt2

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 declare module 'flask-session-cookie-decoder' {
     export function decodeFlaskSessionCookieJson(cookie: string): string;
+    export function decodeFlaskSessionCookieJsonWithZlib(cookie: string): string;
 }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const zlib = require("zlibt2");
+const zlib2 = require("zlibt2");
+import zlib from "zlib"
 const Buffer = typeof window !== 'undefined' ? (window.Buffer || require("buffer").Buffer) : require("buffer").Buffer;
 
 /**
@@ -35,7 +36,7 @@ function decodeFlaskSessionCookieJson(cookie) {
 
     if (compressed) {
         try {
-            const inflate = new zlib.Inflate(Buffer.from(data, 'base64'));
+            const inflate = new zlib2.Inflate(Buffer.from(data, 'base64'));
             const uncompressedData = inflate.decompress();
             decodedData = Buffer.from(uncompressedData).toString('utf-8');
         } catch (error) {
@@ -46,4 +47,43 @@ function decodeFlaskSessionCookieJson(cookie) {
     return decodedData;
 }
 
-module.exports = {decodeFlaskSessionCookieJson};
+function decodeFlaskSessionCookieJsonWithZlib(cookie) {
+    let compressed = false;
+    let payload = cookie;
+
+    if (payload.startsWith('.')) {
+        compressed = true;
+        try {
+            payload = payload.slice(1);
+        } catch (error) {
+            return `[Cookie string is compressed (starts with '.'), but it may be corrupt:  ${error}]`;
+        }
+    }
+
+    const data = payload.split(".")[0];
+    let decodedData = ''
+    // Check if Buffer is available (Node.js environment)
+    if (typeof Buffer !== 'undefined') {
+        try {
+            decodedData = Buffer.from(data, 'base64').toString('utf-8');
+        } catch (error) {
+            return `[base64 decode error:  ${error}]`;
+        }
+    } else {
+        //     TODO: use an alternative method to decode
+        return  '[Error: Buffer class is not defined in the current context.]'
+    }
+
+    if (compressed) {
+        try {
+            const uncompressedData = zlib.inflateSync(Buffer.from(data, 'base64'));
+            decodedData = Buffer.from(uncompressedData).toString('utf-8');
+        } catch (error) {
+            return `[Decompression error:  ${error}]`;
+        }
+    }
+
+    return decodedData;
+}
+
+module.exports = {decodeFlaskSessionCookieJson, decodeFlaskSessionCookieJsonWithZlib};
