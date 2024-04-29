c9c3a6d9f6e9f4751fee72ba18421c456e3e0176
natalia <ewewraw@gmail.com>
2024-04-29 21:03:33 +0200

    fixed decoding errors

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,3 @@
 declare module 'flask-session-cookie-decoder' {
     export function decodeFlaskSessionCookieJson(cookie: string): string;
-    export function decodeFlaskSessionCookieJsonWithZlibModule(cookie: string): string;
-
-    export function decodeFlaskSessionCookieJsonWithZlibjs(cookie: string): string;
-
 }
--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const zlib2 = require("zlibt2");
-const gunzip = require("zlib-module-js/bin/gunzip.min.js");
-const Zlibjs = require('zlibjs')
-const Buffer = typeof window !== 'undefined' ? (window.Buffer || require("buffer").Buffer) : require("buffer").Buffer;
+import pako from "pako";
 
 /**
  * Decodes a Flask session cookie, extracting and decompressing the data stored within it.
@@ -9,122 +6,48 @@ const Buffer = typeof window !== 'undefined' ? (window.Buffer || require("buffer
  * @returns {string} A string representing the decoded JSON object contained within the Flask session cookie.
  */
 function decodeFlaskSessionCookieJson(cookie) {
-    let compressed = false;
-    let payload = cookie;
-
-    if (payload.startsWith('.')) {
-        compressed = true;
-        try {
-            payload = payload.slice(1);
-        } catch (error) {
-            return `[Cookie string is compressed (starts with '.'), but it may be corrupt:  ${error}]`;
-        }
+    if (!cookie) {
+        console.error("Input error: no cookie passed");
+        return "";
     }
 
-    const data = payload.split(".")[0];
-    let decodedData = ''
-    // Check if Buffer is available (Node.js environment)
-    if (typeof Buffer !== 'undefined') {
-        try {
-            decodedData = Buffer.from(data, 'base64').toString('utf-8');
-        } catch (error) {
-            return `[base64 decode error:  ${error}]`;
+    let decodedData = "";
+
+    try {
+        if (cookie.startsWith(".")) {   // if content zipped
+            let data = cookie.split('.')[1].replace(/_/g, '/').replace(/-/g, '+');
+            decodedData = atob(data);
+        } else {
+            let data = cookie.split('.')[0].replace(/_/g, '/').replace(/-/g, '+');
+            decodedData = atob(data);
         }
-    } else {
-    //     TODO: use an alternative method to decode
-        return  '[Error: Buffer class is not defined in the current context.]'
+    } catch (atobError) {
+        console.error("Error decoding base64:", atobError.message);
+        return "Error decoding base64: " + atobError.message;
     }
 
-    if (compressed) {
-        try {
-            const inflate = new zlib2.Inflate(Buffer.from(data, 'base64'));
-            const uncompressedData = inflate.decompress();
-            decodedData = Buffer.from(uncompressedData).toString('utf-8');
-        } catch (error) {
-            return `[Decompression error:  ${error}]`;
+    let binaryData = new Uint8Array(decodedData.length);
+
+    try {
+        for (let i = 0; i < decodedData.length; i++) {
+            binaryData[i] = decodedData.charCodeAt(i);
         }
+    } catch (binaryDataError) {
+        console.error("Error creating binary data:", binaryDataError.message);
+        return "Error creating binary data: " + binaryDataError.message;
     }
 
-    return decodedData;
-}
-
-function decodeFlaskSessionCookieJsonWithZlibModule(cookie) {
-    let compressed = false;
-    let payload = cookie;
-
-    if (payload.startsWith('.')) {
-        compressed = true;
-        try {
-            payload = payload.slice(1);
-        } catch (error) {
-            return `[Cookie string is compressed (starts with '.'), but it may be corrupt:  ${error}]`;
-        }
-    }
-
-    const data = payload.split(".")[0];
-    let decodedData = ''
-    // Check if Buffer is available (Node.js environment)
-    if (typeof Buffer !== 'undefined') {
-        try {
-            decodedData = Buffer.from(data, 'base64').toString('utf-8');
-        } catch (error) {
-            return `[base64 decode error:  ${error}]`;
-        }
-    } else {
-        //     TODO: use an alternative method to decode
-        return  '[Error: Buffer class is not defined in the current context.]'
-    }
-
-    if (compressed) {
-        try {
-            let gunzip_module = new gunzip.Zlib.Gunzip(Buffer.from(data, 'base64'))
-            decodedData = gunzip_module.decompress()
-        } catch (error) {
-            return `[Decompression error:  ${error}]`;
-        }
+    try {
+        decodedData = pako.inflate(binaryData, {to: 'string'});
+    } catch (pakoError) {
+        console.error("Error decompressing data with pako:", pakoError.message);
+        return "Error decompressing data with pako: " + pakoError.message;
     }
 
     return decodedData;
 }
 
-function decodeFlaskSessionCookieJsonWithZlibjs(cookie) {
-    let compressed = false;
-    let payload = cookie;
 
-    if (payload.startsWith('.')) {
-        compressed = true;
-        try {
-            payload = payload.slice(1);
-        } catch (error) {
-            return `[Cookie string is compressed (starts with '.'), but it may be corrupt:  ${error}]`;
-        }
-    }
-
-    const data = payload.split(".")[0];
-    let decodedData = ''
-    // Check if Buffer is available (Node.js environment)
-    if (typeof Buffer !== 'undefined') {
-        try {
-            decodedData = Buffer.from(data, 'base64').toString('utf-8');
-        } catch (error) {
-            return `[base64 decode error:  ${error}]`;
-        }
-    } else {
-        //     TODO: use an alternative method to decode
-        return  '[Error: Buffer class is not defined in the current context.]'
-    }
-
-    if (compressed) {
-        try {
-            let inflate = new Zlib.Inflate(Buffer.from(data, 'base64'));
-            let decompress = inflate.decompress();
-            decodedData = new TextDecoder("utf-8").decode(decompress);
-        } catch (error) {
-            return `[Decompression error:  ${error}]`;
-        }
-    }
-
-    return decodedData;
-}
-
-module.exports = {decodeFlaskSessionCookieJson, decodeFlaskSessionCookieJsonWithZlibModule, decodeFlaskSessionCookieJsonWithZlibjs};
+module.exports = {
+    decodeFlaskSessionCookieJson,
+};
