0526b5dcf56769aeb1cc29610a88fb3f0103bf15
natalia <ewewraw@gmail.com>
2024-04-29 22:08:27 +0200

    removed unused methods

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,3 @@
 declare module 'flask-session-cookie-decoder' {
-    export function decodeFlaskSessionCookieJson(cookie: string): string;
-
-    export function decodeFlaskSessionCookieJsonWithZlibModule(cookie: string): string;
-
-    export function decodeFlaskSessionCookieJsonWithZlibjs(cookie: string): string;
-
     export function getDecodedSessionCookieJson(cookie: string): string;
 }
